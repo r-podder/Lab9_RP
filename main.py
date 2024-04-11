c477407cf313d4a37337140cb7ecb31c50a435fa
peter-ana <166727439+peter-ana@users.noreply.github.com>
2024-04-11 13:27:16 -0400

    main.py

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ print("1. Encode")
 print("2. Decode")
 print("3. Quit")
 option = input("Please enter an option: ")
-if option == 1:
-	password=input("Please enter your password to encode: ")
-	encoded_password=""
-	for digit in password:
-		encoded_digit=str((int(digit)+3)%10)
-		encoded_password+=encoded_digit
-	print("Your password has been encoded and stored!")
+
+if option == '1':
+    password = input("Please enter your password to encode: ")
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    print("Your password has been encoded and stored!")
+
+elif option == '2':
+    encoded_password = input("Please enter your encoded password to decode: ")
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+
+elif option == '3':
+    exit()

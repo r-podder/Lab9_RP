6f598919df5def0e76a6fee0cbd8a6766602d586
unknown <r.podder@ufl.edu>
2024-04-11 10:56:46 -0400

    first addition to repo

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Menu")
+print("-------------")
+print("1. Encode")
+print("2. Decode")
+print("3. Quit")
+option = input("Please enter an option: ")
+if option == 1:
+	password=input("Please enter your password to encode: ")
+	encoded_password=""
+	for digit in password:
+		encoded_digit=str((int(digit)+3)%10)
+		encoded_password+=encoded_digit
+	print("Your password has been encoded and stored!")
